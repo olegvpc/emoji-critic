cca3016ff7a64df48d87e60dce8b75c1cc435439
Oleg Tkach <olegvpc@192.168.1.4>
2023-05-18 10:04:23 +0700

    feat: add README

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
+git init
+git add .
+git ststus
+git fetch origin main
+git push
+
+git commit -m 'first commit'
+
 server for upload files - Web/mongo-files
+git remote add origin https://github.com/olegvpc/emoji-critic.git
+
+olegvpc@192 emoji-critic % git branch  
+* master
+olegvpc@192 emoji-critic % git checkout master 
+M       README.md
+Already on 'master'
+olegvpc@192 emoji-critic % git branch          
+* master
+olegvpc@192 emoji-critic % git branch main master -f 
+olegvpc@192 emoji-critic % git branch                
+  main
+* master
+olegvpc@192 emoji-critic % git checkout main
+M       README.md
+Switched to branch 'main'
+olegvpc@192 emoji-critic % git branch       
+* main
+  master
+olegvpc@192 emoji-critic % git push origin main -f
+Total 0 (delta 0), reused 0 (delta 0), pack-reused 0
+To https://github.com/olegvpc/emoji-critic.git
+ + a226966...b416236 main -> main (forced update)
--- src/components/laravel-data.js
+++ src/components/laravel-data.js
@@ -71,17 +71,122 @@ export const laravelData = [
       timing: {
       }
     },
+    {
+      title: "Полный курс Laravel 10 Компоненты + Вёрстка в Blade (Blade Components)",
+      link: "https://www.youtube.com/embed/StptJsfmv44",
+      linkYouTube: "https://youtu.be/StptJsfmv44",
+      frameName: "9",
+      gitLink: 'https://github.com/indigoram89/laravel-course',
+      timing: {
+      }
+    },
+    {
+      title: "Полный курс Laravel 11 HTTP запросы (Requests)",
+      link: "https://www.youtube.com/embed/wX8RvZc5FBM",
+      linkYouTube: "https://youtu.be/wX8RvZc5FBM",
+      frameName: "10",
+      gitLink: 'https://github.com/indigoram89/laravel-course',
+      timing: {
+      }
+    },
+    {
+      title: "Полный курс Laravel 12 HTTP ответы (Responses)",
+      link: "https://www.youtube.com/embed/8dZj54iWn7U",
+      linkYouTube: "https://youtu.be/8dZj54iWn7U",
+      frameName: "11",
+      gitLink: 'https://github.com/indigoram89/laravel-course',
+      timing: {
+      }
+    },
+    {
+      title: "Полный курс Laravel 13 Сессия (Session)",
+      link: "https://www.youtube.com/embed/zYlAQPXpSqQ",
+      linkYouTube: "https://youtu.be/zYlAQPXpSqQ",
+      frameName: "12",
+      gitLink: 'https://github.com/indigoram89/laravel-course',
+      timing: {
+      }
+    },
+    {
+      title: "Полный курс Laravel 14 База данных + Миграции (Database Migrations)",
+      link: "https://www.youtube.com/embed/xigacHdALng",
+      linkYouTube: "https://youtu.be/xigacHdALng",
+      frameName: "13",
+      gitLink: 'https://github.com/indigoram89/laravel-course',
+      timing: {
+      }
+    },
+    {
+      title: "Полный курс Laravel 15 Модели 1 Основы (Model Basics)",
+      link: "https://www.youtube.com/embed/w7wGnWNnK5c",
+      linkYouTube: "https://youtu.be/w7wGnWNnK5c",
+      frameName: "14",
+      gitLink: 'https://github.com/indigoram89/laravel-course',
+      timing: {
+      }
+    },
+    {
+      title: "Полный курс Laravel 16 Валидация данных (Validation)",
+      link: "https://www.youtube.com/embed/TKL0FWThCeg",
+      linkYouTube: "https://youtu.be/TKL0FWThCeg",
+      frameName: "15",
+      gitLink: 'https://github.com/indigoram89/laravel-course',
+      timing: {
+      }
+    },
+    {
+      title: "Полный курс Laravel 17 Правила валидации + Кейсы (Validation Rules + Examples)",
+      link: "https://www.youtube.com/embed/0NsbSoEQ3Jo",
+      linkYouTube: "https://youtu.be/0NsbSoEQ3Jo",
+      frameName: "16",
+      gitLink: 'https://github.com/indigoram89/laravel-course',
+      timing: {
+      }
+    },
+    {
+      title: "Полный курс Laravel 18 Модели 2 Создание записей в базе данных (Models Creation)",
+      link: "https://www.youtube.com/embed/ZqPSBC5P7vY",
+      linkYouTube: "https://youtu.be/ZqPSBC5P7vY",
+      frameName: "17",
+      gitLink: 'https://github.com/indigoram89/laravel-course',
+      timing: {
+      }
+    },
+    {
+      title: "Полный курс Laravel 19 Модели 3 Получение записей 1 (Models Retrieving)",
+      link: "https://www.youtube.com/embed/g79NMn7HUZg",
+      linkYouTube: "https://youtu.be/g79NMn7HUZg",
+      frameName: "18",
+      gitLink: 'https://github.com/indigoram89/laravel-course',
+      timing: {
+      }
+    },
+    {
+      title: "Полный курс Laravel 20 Модели 4 Получение записей 2 (Models Retrieving)",
+      link: "https://www.youtube.com/embed/Q4iFPt7QPPQ",
+      linkYouTube: "https://youtu.be/Q4iFPt7QPPQ",
+      frameName: "19",
+      gitLink: 'https://github.com/indigoram89/laravel-course',
+      timing: {
+      }
+    },
+    {
+      title: "Полный курс Laravel 21 Модели 5 Условия (Where Clauses)",
+      link: "https://www.youtube.com/embed/nMhyq9b-I-w",
+      linkYouTube: "https://youtu.be/nMhyq9b-I-w",
+      frameName: "20",
+      gitLink: 'https://github.com/indigoram89/laravel-course',
+      timing: {
+      }
+    },
   ]
 
-  
+
   export const getSec = function (timeString) {
     return timeString.split(":").reverse().reduce((prev, item, index) => {
       prev = prev + Number(item) * (index === 0 ? 1 : Math.pow(60, index));
       return prev
     }, 0)
   }
-  
-  
-  
 
-  
+
